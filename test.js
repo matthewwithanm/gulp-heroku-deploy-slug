93a13e508d4c47266c8fff1735db33615d062f0d
Matthew Dapena-Tretter <m@tthewwithanm.com>
2014-09-27 22:00:35 -0400

    Add more tests

--- test.js
+++ test.js
@@ -1,40 +1,56 @@
 var assert = require('assert');
-var gutil = require('gulp-util');
-var deploySlug;
 var sinon = require('sinon');
 var netrc = require('node-netrc');
 var proxyquire = require('proxyquire');
 
+
+var deploySlug;
+var netrcMock = sinon.stub();
+
 describe('deploySlug()', function () {
-  it('errors if netrc is missing creds', function() {
+  before(function() {
     deploySlug = proxyquire('./', {
-      'node-netrc': sinon.stub().returns(false)
+      'node-netrc': netrcMock
     });
-    assert.throws(
-      function() {
-        deploySlug();
-      },
-      "Couldn't get Heroku credentials"
-    );
   });
 
-  deploySlug = require('./');
+  describe('reading user config', function() {
+    before(function() {
+      netrcMock.returns(false);
+    });
 
-  it('errors if app is missing', function() {
-    assert.throws(
-      function() {
-        deploySlug();
-      },
-      'Required option "app" cannot be found'
-    );
+    it('errors if netrc is missing creds', function() {
+      assert.throws(
+        function() {
+          deploySlug({app: 'whatever', slug: {process_types: 'x'}});
+        },
+        /Couldn't get Heroku credentials/
+      );
+    });
   });
 
-  it('errors if process_types is missing', function() {
-    assert.throws(
-      function() {
-        deploySlug({app: 'whatever'});
-      },
-      'Required slug config "process_types" cannot be found.'
-    );
+  describe('missing options', function() {
+    before(function() {
+      netrcMock.returns({login: 'a', password: 'b'});
+    });
+
+    it('errors if app is missing', function() {
+      assert.throws(
+        function() {
+          deploySlug();
+        },
+        /Required option "app" cannot be found/
+      );
+    });
+
+    it('errors if process_types is missing', function() {
+      assert.throws(
+        function() {
+          deploySlug({app: 'whatever'});
+        },
+        'Required slug config "process_types" cannot be found.'
+      );
+    });
   });
+
 });
