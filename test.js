05217ccec890f4f129abcbeea05443dd0b3ea886
Matthew Dapena-Tretter <m@tthewwithanm.com>
2014-09-27 21:33:50 -0400

    Test other bad config

--- test.js
+++ test.js
@@ -6,18 +6,35 @@ var netrc = require('node-netrc');
 var proxyquire = require('proxyquire');
 
 describe('deploySlug()', function () {
-  before(function () {
+  it('errors if netrc is missing creds', function() {
     deploySlug = proxyquire('./', {
       'node-netrc': sinon.stub().returns(false)
     });
-  });
-
-  it('error if netrc is missing creds', function() {
     assert.throws(
       function() {
         deploySlug();
       },
       "Couldn't get Heroku credentials"
+    );
+  });
+
+  deploySlug = require('./');
+
+  it('errors if app is missing', function() {
+    assert.throws(
+      function() {
+        deploySlug();
+      },
+      'Required option "app" cannot be found'
+    );
+  });
+
+  it('errors if process_types is missing', function() {
+    assert.throws(
+      function() {
+        deploySlug({app: 'whatever'});
+      },
+      'Required slug config "process_types" cannot be found.'
     );
   });
 });
